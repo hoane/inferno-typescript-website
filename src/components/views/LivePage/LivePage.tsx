a05cafc4d2c7efb91643df55a2f19e9d155da394
Brandon Hoane <hoanebrandon@gmail.com>
2020-04-05 19:25:27 -0700

    Add https to live stream

--- src/components/views/LivePage/LivePage.tsx
+++ src/components/views/LivePage/LivePage.tsx
@@ -1,6 +1,6 @@
 export const LivePage = () => (
     <div>
         <h1>Hummingbird Cam</h1>
-        <iframe src={"http://local.brandonhoane.com:8081"} width={480} height={640}/>
+        <iframe src={"https://local.brandonhoane.com:8081"} width={480} height={640}/>
     </div>
 );
